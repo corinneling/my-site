3010c5ba64a6c206984afb2554b4001e38a4c680
corinneling <corinne@heysparkbox.com>
2018-05-31 09:34:44 -0400

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+var area = document.querySelector('body');
+var containers = document.querySelector('.parallax');
+    function directionAnimation() {
+        area.addEventListener('mousemove', findDirection); 
+    };
+
+function findDirection(event) {
+    cx = Math.ceil(document.documentElement.scrollHeight / 2.0);
+    cy = Math.ceil(document.documentElement.scrollWidth / 2.0);
+    dx = event.pageX - cx;
+    dy = event.pageY - cy;
+    tiltx = (dy / cy);
+    tilty = - (dx / cx);
+
+    radius = Math.sqrt(Math.pow(tiltx,2) + Math.pow(tilty,2));
+    degree = (radius * 5);
+    
+    containers.style.transform = `
+    rotate3d(${tiltx}, ${tilty}, 0, ${degree}deg)
+    translate(${tiltx}rem, ${tilty}rem)
+    `;
+}
+
+directionAnimation();
