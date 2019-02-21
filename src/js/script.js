02a12acd864e6db7eb488dc0653e3857b056f34f
corinne <corinne@heysparkbox.com>
2019-02-21 14:47:56 -0500

    feat: add a11y control panel

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,90 @@
-var area = document.querySelector('body');
-var containers = document.querySelector('.parallax');
-    function directionAnimation() {
-        area.addEventListener('mousemove', findDirection); 
-    };
-
-function findDirection(event) {
-    cx = Math.ceil(document.documentElement.scrollHeight / 2.0);
-    cy = Math.ceil(document.documentElement.scrollWidth / 2.0);
-    dx = event.pageX - cx;
-    dy = event.pageY - cy;
-    tiltx = (dy / cy);
-    tilty = - (dx / cx);
-
-    radius = Math.sqrt(Math.pow(tiltx,2) + Math.pow(tilty,2));
-    degree = (radius * 5);
-    
-    containers.style.transform = `
-    translate(${tiltx}rem, ${tilty}rem)
-    `;
-}
-
-directionAnimation();
+const aesthetica11y = {
+    openControlPanel: function() {
+      const mainLink = document.querySelector('.aesthetica11y-main-icon');
+      const panelContainer = document.querySelector('.aesthetica11y-container');
+      mainLink.addEventListener('click', () => {
+        panelContainer.style.display == 'block' ? panelContainer.style.display = 'none' : panelContainer.style.display = 'block';
+      });
+    },
+    controlPanelEvents: function() {
+      const links = document.querySelectorAll('.aesthetica11y__button');
+      for (var i = 0; i < links.length; i++) {
+        const properties = [this.prop1, this.prop2, this.prop3, this.prop4, this.prop5, this.prop6, this.prop7, this.prop8, this.prop9];
+        links[i].addEventListener('click', properties[i]);
+      }
+    },
+    prop1: function() {
+      const icons = document.querySelectorAll('.aesthetica11y__button');
+      for (var i = 0; i < icons.length; i++) {
+        icons[i].style.backgroundColor == "" ? icons[i].style.backgroundColor = "#2B2B2B" : icons[i].style.backgroundColor = "";
+      }
+    },
+    prop2: function() {
+      const bdy = document.querySelectorAll("body, div, section, main, aside, footer");
+      const txt = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, span, a");
+      for (var i = 0; i < bdy.length; i++) {
+        if (bdy[i].style.backgroundColor == 'black') {
+          bdy[i].style.backgroundImage = "none";
+          bdy[i].style.backgroundColor = 'white';
+          bdy[i].style.color = 'black';
+          txt[i].style.color = 'black';
+        } else {
+          bdy[i].style.backgroundImage = "none";
+          bdy[i].style.backgroundColor = 'black';
+          bdy[i].style.color = 'white';
+          txt[i].style.color = 'white';
+        }
+      }
+    },
+    prop3: function() {
+      const txt = document.querySelectorAll("p, li, a, span")
+      let childrenFont, currentSize;
+      for (let i = 0; i < txt.length; i++) {
+        childrenFont = window.getComputedStyle(txt[i]).getPropertyValue('font-size');
+        currentSize = parseFloat(childrenFont);
+        txt[i].style.fontSize = (currentSize - 2) + 'px';
+      }
+    },
+    prop4: function() {
+      const txt = document.querySelectorAll("p, li, a, span")
+      let childrenFont, currentSize;
+      for (let i = 0; i < txt.length; i++) {
+        childrenFont = window.getComputedStyle(txt[i]).getPropertyValue('font-size');
+        currentSize = parseFloat(childrenFont);
+        txt[i].style.fontSize = (currentSize + 2) + 'px';
+      }
+    },
+    prop5: function() {
+      const a = document.querySelectorAll("a")
+      for (let i = 0; i < a.length; i++) {
+        a[i].style.textDecoration == "" ? a[i].style.textDecoration = "underline" : a[i].style.textDecoration = "";
+      }
+    },
+    prop6: function() {
+      const txt = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, li, a, span");
+      for (let i = 0; i < txt.length; i++) {
+        txt[i].style.letterSpacing == "" ? txt[i].style.letterSpacing = "1px" : txt[i].style.letterSpacing = "";
+      }
+    },
+    prop7: function() {
+      const txt = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, li, a, span");
+      for (let i = 0; i < txt.length; i++) {
+        txt[i].style.lineHeight == "" ? txt[i].style.lineHeight = "1.75" : txt[i].style.lineHeight = "";
+      }
+    },
+    prop8: function() {
+      const txt = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, li, a, span");
+      for (let i = 0; i < txt.length; i++) {
+        txt[i].style.fontFamily == "" ? txt[i].style.fontFamily = "Dyslexie" : txt[i].style.fontFamily = "" 
+      }
+    },
+    prop9: function() {
+      const els = document.querySelectorAll("*");
+      els.forEach((el) => {
+        el.removeAttribute("style");
+      });
+    }
+  }
+  
+  aesthetica11y.openControlPanel();
+  aesthetica11y.controlPanelEvents();
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
   plugins: [
     new MinifyPlugin(),
     new CopyWebpackPlugin([
-      {from:'src/images',to:'../images'} 
+      {from:'src/images',to:'../images'},
+      {from:'src/font',to:'../font'} 
     ])
   ]
 };
