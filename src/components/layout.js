974a755461200bb22e41f29950b079a621dad3c0
corinne <corinne@heysparkbox.com>
2019-07-31 13:25:14 -0400

    fix: change scss import from dynamic to static

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "../components/header/header";
-import("../scss/base.scss");
+import "../scss/base.scss"
 
  const Layout = ({ children }) => (
   <div className="l--site-container">
